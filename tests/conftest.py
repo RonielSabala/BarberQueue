737ff132c290dba85a45216a2b1e87afd8d296f3
RonielSabala <ronielsabala11@gmail.com>
2026-02-07 14:42:42 -0400

    Use typehints in tests module

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,25 @@
+from collections.abc import Iterator
 from pathlib import Path
 
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chromium.webdriver import ChromiumDriver
 
 APP_URL = "http://localhost:3000"
 SCREENSHOTS_DIR = Path(__file__).resolve().parent / "results" / "captures"
 
 
-def pytest_addoption(parser):
+def pytest_addoption(parser: pytest.Parser) -> None:
     parser.addoption("--base-url", action="store", default=APP_URL, help="Base app URL")
 
 
 @pytest.fixture(scope="session")
-def base_url(request):
+def base_url(request: pytest.FixtureRequest) -> str:
     return request.config.getoption("base_url")
 
 
 @pytest.fixture(scope="function")
-def driver(request):
+def driver(request: pytest.FixtureRequest) -> Iterator[ChromiumDriver]:
     driver = webdriver.Chrome()
     driver.implicitly_wait(5)
     yield driver
@@ -25,7 +27,7 @@ def driver(request):
 
 
 @pytest.fixture(scope="function")
-def capture_dir(request):
+def capture_dir(request: pytest.FixtureRequest) -> str:
     test_name = request.node.name
     story = Path(request.node.fspath).parent.name
     base = SCREENSHOTS_DIR / story / test_name
--- tests/views/test_home.py
+++ tests/views/test_home.py
@@ -1,16 +1,17 @@
 import time
 
 import pytest
+from selenium.webdriver.chromium import webdriver
 
 
 @pytest.mark.usefixtures("driver", "base_url", "capture_dir")
-def test_hello_world(driver, base_url, capture_dir):
+def test_hello_world(
+    driver: webdriver.ChromiumDriver, base_url: str, capture_dir: str
+) -> None:
     """
     Test for the home page.
     """
 
     driver.get(f"{base_url}/home.php")
-
     time.sleep(1)
-
-    driver.save_screenshot(f"{capture_dir}/hello_world_msg.png")
+    driver.save_screenshot(f"{capture_dir}/hello_world_message.png")
