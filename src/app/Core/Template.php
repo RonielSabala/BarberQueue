66289141bdba735f3955d80cf11728605146b93b
RonielSabala <ronielsabala11@gmail.com>
2026-02-14 18:50:01 -0400

    Implement a function that searchs a file in different directory layers

--- src/app/Core/Template.php
+++ src/app/Core/Template.php
@@ -9,23 +9,22 @@ use Domain\HeaderType\HeaderType;
 class Constants
 {
     private const VIEWS_DIR_NAME = 'views';
-    public const CSS_DIR_NAME = 'css';
-    public const JS_DIR_NAME = 'js';
-    private const PARTIALS_DIR_NAME = '_partials';
-
     public const VIEWS_FILE_EXT = '.php';
+
+    public const CSS_DIR_NAME = 'css';
     public const CSS_FILE_EXT = '.css';
+
+    public const JS_DIR_NAME = 'js';
     public const JS_FILE_EXT = '.js';
 
-    private const PUBLIC_DIR = SRC_DIR . '/' . 'public';
-    public const VIEWS_DIR = self::PUBLIC_DIR . '/' . self::VIEWS_DIR_NAME;
-    public const CSS_DIR = self::PUBLIC_DIR . '/' . self::CSS_DIR_NAME;
-    public const JS_DIR = self::PUBLIC_DIR . '/' . self::JS_DIR_NAME;
-
+    public const PARTIALS_DIR_NAME = '_partials';
     public const PARTIAL_HEADER_FILENAME = self::PARTIALS_DIR_NAME . '/' . '_header';
     public const PARTIAL_NAV_FILENAME = self::PARTIALS_DIR_NAME . '/' . '_nav';
     public const PARTIAL_FOOTER_FILENAME = self::PARTIALS_DIR_NAME . '/' . '_footer';
 
+    public const VIEWS_DIR = PUBLIC_DIR . '/' . self::VIEWS_DIR_NAME;
+    public const CSS_DIR = PUBLIC_DIR . '/' . self::CSS_DIR_NAME;
+    public const JS_DIR = PUBLIC_DIR . '/' . self::JS_DIR_NAME;
 }
 
 class Template
@@ -59,29 +58,57 @@ class Template
         }
     }
 
+    private function findRelativeFilePath(string $baseDir, string $file): string | null
+    {
+        $dirParts = explode('/', self::$viewDir);
+
+        $lastElement = '';
+        while ($lastElement !== null) {
+            $joinedDir = join('/', $dirParts);
+            if ($joinedDir !== '') {
+                $joinedDir .= '/';
+            }
+
+            $testDir = $baseDir .  '/' . $joinedDir . $file;
+
+            if (!file_exists($testDir)) {
+                $lastElement = array_pop($dirParts);
+                continue;
+            }
+
+            return $joinedDir . $file;
+        }
+
+        return null;
+    }
+
     private function getViewFilePath(string $viewFilename): string
     {
-        return Constants::VIEWS_DIR . '/' . self::$viewDir . '/' . $viewFilename . Constants::VIEWS_FILE_EXT;
+        $viewFile = $viewFilename . Constants::VIEWS_FILE_EXT;
+        $relFilePath = self::findRelativeFilePath(Constants::VIEWS_DIR, $viewFile);
+        return Constants::VIEWS_DIR . '/' . $relFilePath;
     }
 
     private function getCSSLink(string $cssFilename): string
     {
-        $filePath = self::$viewDir . (self::$viewDir === '' ? '' : '/') . $cssFilename . Constants::CSS_FILE_EXT;
-        if (!file_exists(Constants::CSS_DIR . '/' . $filePath)) {
+        $cssFile = $cssFilename . Constants::CSS_FILE_EXT;
+        $relFilePath = self::findRelativeFilePath(Constants::CSS_DIR, $cssFile);
+        if ($relFilePath === null) {
             return '';
         }
 
-        return '<link rel="stylesheet" href="/' . Constants::CSS_DIR_NAME . '/' . $filePath . '">' . "\n";
+        return '<link rel="stylesheet" href="/' . Constants::CSS_DIR_NAME . '/' . $relFilePath . '">' . "\n";
     }
 
     private function getJSScript(string $jsFilename): string
     {
-        $filePath = self::$viewDir . (self::$viewDir === '' ? '' : '/') . $jsFilename . Constants::JS_FILE_EXT;
-        if (!file_exists(Constants::JS_DIR . '/' . $filePath)) {
+        $jsFile = $jsFilename . Constants::JS_FILE_EXT;
+        $relFilePath = self::findRelativeFilePath(Constants::JS_DIR, $jsFile);
+        if ($relFilePath === null) {
             return '';
         }
 
-        return "\n" . '<script src="/' . Constants::JS_DIR_NAME . '/' . $filePath . '"></script>';
+        return "\n" . '<script src="/' . Constants::JS_DIR_NAME . '/' . $relFilePath . '"></script>';
     }
 
     public function __construct()
