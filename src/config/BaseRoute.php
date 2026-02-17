e45dc4c7206834d1eb8c4349d5db6918fb19a2b6
RonielSabala <ronielsabala11@gmail.com>
2026-02-17 16:33:31 -0400

    Refactor Route class into a different file

---
+++ src/config/BaseRoute.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Config;
+
+use App\Controllers\BaseController;
+
+class BaseRoute
+{
+    public function __construct(
+        public readonly string $viewRoute,
+        public readonly BaseController $viewController,
+    ) {}
+
+    public function splitViewRoute(): array
+    {
+        $parts = explode('/', $this->viewRoute);
+        if (\count($parts) === 1) {
+            return ['', $this->viewRoute];
+        }
+
+        $viewDir = implode('/', \array_slice($parts, 0, -1));
+        $viewName = end($parts);
+        return [$viewDir, $viewName];
+    }
+}
--- src/config/WebRoutes.php
+++ src/config/WebRoutes.php
@@ -6,37 +6,17 @@ namespace Config;
 
 use App\Controllers\BaseController;
 
-class Route
-{
-    public function __construct(
-        public readonly string $viewRoute,
-        public readonly BaseController $viewController,
-    ) {}
-
-    public function splitViewRoute(): array
-    {
-        $parts = explode('/', $this->viewRoute);
-        if (\count($parts) === 1) {
-            return ['', $this->viewRoute];
-        }
-
-        $viewDir = implode('/', \array_slice($parts, 0, -1));
-        $viewName = end($parts);
-        return [$viewDir, $viewName];
-    }
-}
-
 class WebRoutes
 {
     private static array $routes = [];
 
     public static function add(string $viewRoute, BaseController $viewController): void
     {
-        $route = new Route($viewRoute, $viewController);
+        $route = new BaseRoute($viewRoute, $viewController);
         self::$routes[$route->viewRoute] = $route;
     }
 
-    public static function getByUri(string $viewRoute): ?Route
+    public static function getByUri(string $viewRoute): ?BaseRoute
     {
         return self::$routes[$viewRoute] ?? null;
     }
