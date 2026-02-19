306d958e807784cdbb57d7d7dde32b96a8ae5cb5
RonielSabala <ronielsabala11@gmail.com>
2026-02-19 10:20:41 -0400

    Rename src to backend

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -6,7 +6,7 @@ use PhpCsFixer\{Config, Finder};
 use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
 
 $finder = Finder::create()
-    ->in([__DIR__ . '/src'])
+    ->in([__DIR__ . '/backend'])
     ->name('*.php')
     ->ignoreDotFiles(true)
     ->ignoreVCS(true)
