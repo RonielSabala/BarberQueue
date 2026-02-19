41415fb06a3d2adcaeac3db60e29c779627c3218
RonielSabala <ronielsabala11@gmail.com>
2026-02-19 12:55:56 -0400

    Add cors

--- backend/index.php
+++ backend/index.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 require_once __DIR__ . '/bootstrap.php';
 
 use App\Core\Router;
+use App\Middleware\CorsMiddleware;
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
 (new Router())->dispatch();
+CorsMiddleware::handle();
