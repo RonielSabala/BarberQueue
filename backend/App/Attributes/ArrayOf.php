82f40209e5564f27ad514057b592cb86ff159f6b
RonielSabala <ronielsabala11@gmail.com>
2026-04-07 11:46:31 -0400

    Make all attributes final readonly

--- backend/App/Attributes/ArrayOf.php
+++ backend/App/Attributes/ArrayOf.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Attributes;
 
 #[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_PARAMETER)]
-class ArrayOf
+final readonly class ArrayOf
 {
     public function __construct(
         public readonly string $type,
--- backend/App/Attributes/RoutePrefix.php
+++ backend/App/Attributes/RoutePrefix.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Attributes;
 
 #[\Attribute(\Attribute::TARGET_CLASS)]
-class RoutePrefix
+final readonly class RoutePrefix
 {
     public function __construct(public readonly string $prefix) {}
 }
